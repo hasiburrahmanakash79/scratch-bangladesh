64c1649d66c551937fe457ea3a8e11cb18f45dd2
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-04-01 13:11:06 +0600

    create translator for blog section

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,12 @@
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 const Blog = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="container pt-28 mx-auto p-3">
-      <h1 className="md:text-5xl text-3xl font-bold pb-7 text-center">Blog</h1>
+      <h1 className="md:text-5xl text-3xl font-bold pb-7 text-center">{t("Blog")}</h1>
 
       <div className=" md:grid grid-cols-3 my-5 gap-5 ">
         <div className="bg-[#ededed] p-5 rounded-md shadow-lg">
@@ -13,7 +16,7 @@ const Blog = () => {
             </div>
           </div>
             <p className="text-xl font-semibold my-3">1. স্ক্র্যাচে এখন থেকে বাংলাতেই প্রোগ্রামিং শেখা যাবে</p>
-          <p>জনপ্রিয় স্ক্র্যাচ প্ল্যাটফর্মে এখন থেকে বাংলা ভাষাতেই প্রোগ্রামিং ও প্রজেক্ট করা যাবে। বিশ্বের ১৯০টি দেশের শিশু-কিশোরদের প্রোগ্রামিং শেখার...... <Link to="/details" className="link link-hover text-violet-600">Read more</Link></p>
+          <p>জনপ্রিয় স্ক্র্যাচ প্ল্যাটফর্মে এখন থেকে বাংলা ভাষাতেই প্রোগ্রামিং ও প্রজেক্ট করা যাবে। বিশ্বের ১৯০টি দেশের শিশু-কিশোরদের প্রোগ্রামিং শেখার...... <Link to="/details" className="link link-hover text-violet-600">{t("Read more")}</Link></p>
         </div>
         <div className="bg-[#ededed] p-5 rounded-md shadow-lg">
           <div className="avatar">
@@ -22,7 +25,7 @@ const Blog = () => {
             </div>
           </div>
             <p className="text-xl font-semibold my-3">1. স্ক্র্যাচে এখন থেকে বাংলাতেই প্রোগ্রামিং শেখা যাবে</p>
-          <p>জনপ্রিয় স্ক্র্যাচ প্ল্যাটফর্মে এখন থেকে বাংলা ভাষাতেই প্রোগ্রামিং ও প্রজেক্ট করা যাবে। বিশ্বের ১৯০টি দেশের শিশু-কিশোরদের প্রোগ্রামিং শেখার...... <Link to="/details" className="link link-hover text-violet-600">Read more</Link></p>
+          <p>জনপ্রিয় স্ক্র্যাচ প্ল্যাটফর্মে এখন থেকে বাংলা ভাষাতেই প্রোগ্রামিং ও প্রজেক্ট করা যাবে। বিশ্বের ১৯০টি দেশের শিশু-কিশোরদের প্রোগ্রামিং শেখার...... <Link to="/details" className="link link-hover text-violet-600">{t("Read more")}</Link></p>
         </div>
         <div className="bg-[#ededed] p-5 rounded-md shadow-lg">
           <div className="avatar">
@@ -31,7 +34,7 @@ const Blog = () => {
             </div>
           </div>
             <p className="text-xl font-semibold my-3">1. স্ক্র্যাচে এখন থেকে বাংলাতেই প্রোগ্রামিং শেখা যাবে</p>
-          <p>জনপ্রিয় স্ক্র্যাচ প্ল্যাটফর্মে এখন থেকে বাংলা ভাষাতেই প্রোগ্রামিং ও প্রজেক্ট করা যাবে। বিশ্বের ১৯০টি দেশের শিশু-কিশোরদের প্রোগ্রামিং শেখার...... <Link to="/details" className="link link-hover text-violet-600">Read more</Link></p>
+          <p>জনপ্রিয় স্ক্র্যাচ প্ল্যাটফর্মে এখন থেকে বাংলা ভাষাতেই প্রোগ্রামিং ও প্রজেক্ট করা যাবে। বিশ্বের ১৯০টি দেশের শিশু-কিশোরদের প্রোগ্রামিং শেখার...... <Link to="/details" className="link link-hover text-violet-600">{t("Read more")}</Link></p>
         </div>
       </div>
     </div>
