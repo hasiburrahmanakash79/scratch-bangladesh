f07a97500eaee94c12cf39c3531b05ef820b4afb
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-04-05 23:02:27 +0600

    make quiz section for test

--- src/Pages/ResultPage.jsx
+++ src/Pages/ResultPage.jsx
@@ -4,7 +4,7 @@ const ResultPage = () => {
   
     return (
       <div>
-        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ipsum veniam provident laudantium harum exercitationem excepturi magni, velit fugit ex neque, atque eligendi deserunt, in ipsa quis. Voluptates aliquid rem architecto earum blanditiis expedita? Distinctio laboriosam corrupti officia doloremque laudantium beatae nesciunt tenetur fugiat, ea ipsum quam atque omnis enim!</p>
       </div>
     );
   };
