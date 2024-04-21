4bf0971512d231742741390f40be969be49184e9
Abu Ahamed Rafi <abuahamedrafi@gmail.com>
2024-04-21 12:57:08 +0600

    Content Change Translation

--- src/Pages/Olympiad/Olympiad.jsx
+++ src/Pages/Olympiad/Olympiad.jsx
@@ -56,7 +56,7 @@ const Olympiad = () => {
       </div> */}
       <div>
         <div className="my-10">
-          <h1 className="text-3xl font-bold mb-7">Olympiad Photo Gallery: </h1>
+          <h1 className="text-3xl font-bold mb-7">{t("Olympiad Photo Gallery:")} </h1>
           <div className="grid md:grid-cols-5 grid-cols-2 gap-5 ">
             {/* <div className="relative">
               <img
