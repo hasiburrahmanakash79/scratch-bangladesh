703c555d0e3b7d744f6442ed081f135e35c8605b
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-04-23 14:55:16 +0600

    change fontfamily

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => {
           <h1 className="my-5 md:text-5xl text-3xl font-bold">
             {t("Scratch Bangladesh")}
           </h1>
-          <p className="leading-7">
+          <p className="leading-8">
             {t("Learning programming is popular among elementary school students Scratch Bangladesh is bringing a new way to do this. scratch A block-base programming language, along with Scratch for kids and teens Useful The world's largest programming community. using scratch Programming logic is very easy to learn and digital stories, animation videos,Games etc. projects can be created. Games, animations, presentations,Graphics art work, interactive stories etc. are of different types Everything needed to create a project is easily possible in Scratch. here Rather than writing code by typing on the keyboard like in other programming languages Using only blocks to perform tasks, learning from scratch is easier. Scratch.mit.edu website in more than 80 languages Programming can be done, but now in Bengali language as Bengali is added Coding is possible. Scratch Bangladesh on scratch in Bengali language Tutorials, Project Ideas, Courses etc. Various events throughout the year organizes Scratch Bangladesh is our country's primary school A new milestone for students, who are new to learning programming will open the door")}
           </p>
         </div>
