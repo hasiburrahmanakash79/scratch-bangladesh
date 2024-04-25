44f36863fa90f5de5ec85009777ff6a0a4a89df5
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-04-26 00:03:48 +0600

    remove lorem

--- src/Pages/Dashboard/AddGallery/AddGallery.jsx
+++ src/Pages/Dashboard/AddGallery/AddGallery.jsx
@@ -3,7 +3,7 @@ const AddGallery = () => {
     return (
         <div>
             <h1>Add Gallery</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic magnam harum, amet magni non maiores, fugit porro molestiae excepturi blanditiis minus odit soluta libero qui eligendi nemo a mollitia reprehenderit! Molestias iusto accusamus explicabo assumenda, veniam enim quam optio culpa placeat dolore atque eum dolores fuga, saepe deleniti debitis sunt autem, asperiores distinctio in reiciendis aperiam omnis quae.</p>
+            
         </div>
     );
 };
--- src/Pages/Dashboard/CreateFeatures/CreateFeatures.jsx
+++ src/Pages/Dashboard/CreateFeatures/CreateFeatures.jsx
@@ -4,7 +4,7 @@ const CreateFeatures = () => {
     return (
         <div>
             <h1>CreateFeatures</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum quae soluta molestias modi sint. Ratione, deleniti cumque et esse nesciunt est voluptate, beatae ducimus expedita ea autem officia illo sequi saepe! Debitis mollitia autem dolorum nemo veniam consectetur placeat rerum? Animi quaerat perferendis tempore cumque officia, odit eos aperiam similique!</p>
+            
         </div>
     );
 };
