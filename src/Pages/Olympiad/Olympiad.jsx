68af0b4f2e146e9a8da6edbe35fe206012984f23
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-19 15:43:59 +0600

    try to implement photigallery

--- src/Pages/Olympiad/Olympiad.jsx
+++ src/Pages/Olympiad/Olympiad.jsx
@@ -1,49 +1,75 @@
+import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
-
-
 const Olympiad = () => {
   const { t } = useTranslation();
-  
   const data = [
     {
-      imageLink:
+      imgelink:
         "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     },
     {
-      imageLink:
+      imgelink:
         "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     },
     {
-      imageLink:
-        "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    },
-    {
-      imageLink:
+      imgelink:
         "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
     },
     {
-      imageLink:
+      imgelink:
         "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
     },
     {
-      imageLink:
+      imgelink:
         "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
     },
     {
-      imageLink:
-        "https://demos.creative-tim.com/material-kit-pro/assets/img/examples/blog5.jpg",
+      imgelink:
+        "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
     },
     {
-      imageLink:
-        "https://material-taillwind-pro-ct-tailwind-team.vercel.app/img/content2.jpg",
+      imgelink:
+        "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
     },
     {
-      imageLink:
-        "https://images.unsplash.com/photo-1620064916958-605375619af8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1493&q=80",
+      imgelink:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      imgelink:
+        "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     },
   ];
+
+  const [active, setActive] = React.useState(
+    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+  );
 
   return (
     <div className="container pt-28 mx-auto p-3">
@@ -90,7 +116,10 @@ const Olympiad = () => {
               <h1>Scratch Olympiad 2024 (Upcomming...)</h1>
             </div>
             <div className="p-2 bg-white rounded-md shadow-lg link link-hover hover:bg-blue-300">
-              <a href="https://drive.google.com/file/d/1irZJJC3QRJYDfvgO_OYrujK4Y_GOW-tX/view?usp=sharing" target="_blank">
+              <a
+                href="https://drive.google.com/file/d/1irZJJC3QRJYDfvgO_OYrujK4Y_GOW-tX/view?usp=sharing"
+                target="_blank"
+              >
                 Scratch Olympiad 2023
               </a>
             </div>
@@ -132,24 +161,47 @@ const Olympiad = () => {
           </div>
         </div>
       </div>
-      
+
       <div>
         <div className="my-10">
           <h1 className="text-3xl font-bold mb-7">
             {t("Olympiad Photo Gallery:")}{" "}
           </h1>
           <div className="grid md:grid-cols-5 grid-cols-2 gap-5 ">
-            
-          {data.map(({ imageLink }, index) => (
-        <Link key={index}>
-          <img
-            className="h-40 w-full max-w-full hover:scale-150 hover:z-50 duration-300 rounded-lg object-cover object-center"
-            src={imageLink}
-            alt="gallery-photo"
-          />
-        </Link>
-      ))}
+            {data.map(({ imgelink }, index) => (
+              <Link key={index}>
+                <img
+                  className="h-40 w-full max-w-full hover:scale-150 hover:z-50 duration-300 rounded-lg object-cover object-center"
+                  src={imgelink}
+                  alt="gallery-photo"
+                />
+              </Link>
+            ))}
           </div>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img
+            className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
+            src={active}
+            alt=""
+          />
+        </div>
+        <div className="flex gap-4">
+          {data.map(({ imgelink }, index) => (
+            <div
+              key={index}
+              className=" "
+            >
+              <img
+                onClick={() => setActive(imgelink)}
+                src={imgelink}
+                className="h-20  max-w-full cursor-pointer rounded-lg object-cover object-center"
+                alt="gallery-image"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
