10cf91199c2e54865f281d4409cd0dec1fe75f40
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-22 23:40:23 +0600

    make another course card

--- src/Pages/HomePage/CourseCard/CourseCard.jsx
+++ src/Pages/HomePage/CourseCard/CourseCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { Card, CardHeader } from "@material-tailwind/react";
 const CourseCard = () => {
   const { t } = useTranslation();
   return (
@@ -44,7 +45,7 @@ const CourseCard = () => {
             <div className="items-center">
               <div className="">
                 <img
-                  src="https://scratch.mit.edu/images/scratch-og.png"
+                  src="https://www.learningresources.co.uk/media/home_uk/lruk-blog-images/What-is-Block-Coding-for-Kids_Header.jpg"
                   className=" rounded-xl"
                   alt=""
                 />
@@ -70,6 +71,41 @@ const CourseCard = () => {
             </div>
           </div>
         </div>
+
+        <Card className="overflow-hidden bg-slate-50 border p-4 hover:shadow-xl">
+            <CardHeader
+              floated={false}
+              shadow={false}
+              color="transparent"
+              className="m-0 rounded-none"
+            >
+              <div className="relative h-64 rounded overflow-hidden">
+                <img
+                  src="https://www.learningresources.co.uk/media/home_uk/lruk-blog-images/What-is-Block-Coding-for-Kids_Header.jpg"
+                  alt="{card.title}"
+                  className="w-full h-full object-cover transition-transform duration-300 hover:scale-110"
+                />
+              </div>
+            </CardHeader>
+            <div className="md:flex justify-between items-center">
+                <div className="text-center text-black md:text-left mb-5">
+                  <h1 className="my-3 md:text-3xl text-2xl font-semibold uppercase">
+                    {t("Scratch programming for everyone")}
+                  </h1>
+                  <p>
+                    {t("Categories")}: {t("Scratch Basic")}
+                  </p>
+                  <p className="text-sm opacity-40">
+                    {t("video tutorial")} | {t("Paid")}
+                  </p>
+                </div>
+                <div className="flex justify-center items-center">
+                  <Link className=" btn text-xl btn-primary " to="/">
+                    {t("Start Now")}
+                  </Link>
+                </div>
+              </div>
+          </Card>
       </div>
     </div>
   );
